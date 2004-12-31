be7314819ceced8b0ed5df9aaa75ae412d99140c
lcr11 <qinlknkndnahtnbjliqotglh@yahoo.com>
2005-01-01 02:29:20 +0800

    submit authoritactive figures

---
+++ 3-6/3-6/commentsv0.1.201611262207/detail/js/newscontent.js
@@ -0,0 +1,5 @@
+import '../../asset/style/style1.css';
+import '../../asset/style/style1.debug.css';
+import '../../asset/style/style.news.v1.css';
+import NewsContent from '../js/content.js';
+ReactDOM.render(<NewsContent />,document.getElementById('content'));
