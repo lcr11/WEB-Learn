2f971f672d38dcc1c1976cc062ae43deab7d46b6
lcr11 <qinlknkndnahtnbjliqotglh@yahoo.com>
2017-08-14 22:28:18 +0800

    submit authoritactive figures

--- 3-6/3-6/commentsv0.1.201611262207/detail/js/newscontent.js
+++
@@ -1,5 +0,0 @@
-import '../../asset/style/style1.css';
-import '../../asset/style/style1.debug.css';
-import '../../asset/style/style.news.v1.css';
-import NewsContent from '../js/content.js';
-ReactDOM.render(<NewsContent />,document.getElementById('content'));
