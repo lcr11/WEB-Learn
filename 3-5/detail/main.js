67aff160205f76613d559f13fa5954a23c9172de
lcr11 <icrinivkny@163.com>
2017-08-19 22:52:28 +0800

    Delete main.js

--- 3-5/detail/main.js
+++
@@ -1,7 +0,0 @@
-import '../asset/style/global.css';
-//import '../js/template.js';
-//import qqlogo from  '../asset/js';
-//console.log(qqlogo);
-//$('body').html('<img src="'+qqlogo+'">');
-
-import './detail.js';
