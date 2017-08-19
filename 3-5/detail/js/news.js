0958959cd71f4784ef3ab41f387c54ebe92aa03a
lcr11 <icrinivkny@163.com>
2017-08-19 23:04:14 +0800

    Delete news.js

--- 3-5/detail/js/news.js
+++
@@ -1,7 +0,0 @@
-import  '../../asset/style/style1.css';
-import  '../../asset/style/style1.debug.css';
-import  NewsList from './newslist.js';
-    ReactDOM.render(
-          <NewsList />,
-          document.getElementById('content')
-		);
